cd7cfbff98a1c2fc03a76c343b9d5a71fb7aeb8d
Junaid Girkar <60307291+junaidgirkar@users.noreply.github.com>
2021-12-29 16:38:43 +0530

    Second commit

--- authentication/models.py
+++ authentication/models.py
@@ -17,8 +17,8 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, first_name, last_name, password):
-        user = self.create_user(email, first_name, last_name, password)
+    def create_superuser(self, email, first_name, last_name,sap_id, password):
+        user = self.create_user(email, first_name, last_name, sap_id, password)
         user.staff = True
         user.admin = True
         user.save(using=self._db)
@@ -38,7 +38,7 @@ class User(AbstractBaseUser):
 
     objects     = UserManager()
     USERNAME_FIELD  = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'sap_id']
     
     def __str__(self):
         return self.email
